76e39d0a088cd54b4e8ea75d8be3b267ec8f7e3a
Appz-dk <patrick.fyhn@gmail.com>
2023-04-26 23:03:05 +0200

    Added toggleFnc to prevent page scrolling when menu is open

--- src/components/header/nav/Nav.tsx
+++ src/components/header/nav/Nav.tsx
@@ -6,9 +6,24 @@ import NavList from "./NavList";
 
 const Nav = () => {
   const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu((currentMenuState) => {
+      // Prevent scrolling on page if menu is being opened
+      // aka. currentMenuState === false
+      if (!currentMenuState) {
+        document.body.style.overflow = "hidden";
+      } else {
+        document.body.style.overflow = "unset";
+      }
+      // Toggle currentMenuState
+      return !currentMenuState;
+    });
+  };
+
   return (
     <nav className={classes.nav}>
-      <div className={classes.backdrop} data-show={showMenu} onClick={() => setShowMenu(false)} />
+      <div className={classes.backdrop} data-show={showMenu} onClick={toggleMenu} />
       <Logo />
       {/* Navigation list */}
       <Hamburger
@@ -16,7 +31,7 @@ const Nav = () => {
         aria-label="hamburger menu"
         aria-expanded={showMenu}
         className={classes.hamburger}
-        onClick={() => setShowMenu((prev) => !prev)}
+        onClick={toggleMenu}
       />
       <ul role="list" data-show={showMenu} className={classes["nav-list"]}>
         <NavList />
