eb184ee73cfc47c0938a849aacccd7d4ed2fe455
Appz-dk <patrick.fyhn@gmail.com>
2023-04-26 22:51:01 +0200

    Added menu backdrop & refactored NavList into seperate component

--- src/components/header/nav/Nav.tsx
+++ src/components/header/nav/Nav.tsx
@@ -2,11 +2,13 @@ import { ReactComponent as Logo } from "../../../assets/images/logo.svg";
 import { ReactComponent as Hamburger } from "../../../assets/images/icon-hamburger.svg";
 import classes from "./Nav.module.css";
 import { useState } from "react";
+import NavList from "./NavList";
 
 const Nav = () => {
   const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className={classes.nav}>
+      <div className={classes.backdrop} data-show={showMenu} onClick={() => setShowMenu(false)} />
       <Logo />
       {/* Navigation list */}
       <Hamburger
@@ -17,18 +19,7 @@ const Nav = () => {
         onClick={() => setShowMenu((prev) => !prev)}
       />
       <ul role="list" data-show={showMenu} className={classes["nav-list"]}>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <button>Contact</button>
-        </li>
+        <NavList />
       </ul>
     </nav>
   );
