bf7e674c668f5e2ad99e91f7642a96391df5353a
Appz-dk <patrick.fyhn@gmail.com>
2023-04-26 22:51:15 +0200

    Refactored nav items into seperate component

---
+++ src/components/header/nav/NavList.tsx
@@ -0,0 +1,20 @@
+const NavList = () => {
+  return (
+    <>
+      <li>
+        <a href="#about">About</a>
+      </li>
+      <li>
+        <a href="#services">Services</a>
+      </li>
+      <li>
+        <a href="#projects">Projects</a>
+      </li>
+      <li>
+        <button>Contact</button>
+      </li>
+    </>
+  );
+};
+
+export default NavList;
